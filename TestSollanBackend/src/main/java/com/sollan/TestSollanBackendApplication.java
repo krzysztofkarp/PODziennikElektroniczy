04f45ce322ee41f7336b2dfa0cf5499ad40cf613
Aleksander Smal <asmal@PC-1610.sollan.exchange>
2017-10-04 09:53:43 +0200

    Test Sollan Backend Init

---
+++ TestSollanBackend/src/main/java/com/sollan/TestSollanBackendApplication.java
@@ -0,0 +1,16 @@
+package com.sollan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author asmal
+ *
+ */
+@SpringBootApplication
+public class TestSollanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSollanBackendApplication.class, args);
+	}
+}
